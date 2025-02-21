ed3d99a1f6c4e5058c89788ddc6830f11d345e74
simbaclaws <github@hylke.it>
2025-02-21 21:42:27 +0100

    Change some dmenu and dwm settings

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#000000", "#53b3a2" },
+	[SchemeSel] = { "#000000", "#50cad2" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
