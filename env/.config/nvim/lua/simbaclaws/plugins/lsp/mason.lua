427d1d34fa349fc55accc9ddd41bc338fecb7bce
simbaclaws <github@hylke.it>
2025-02-21 23:26:16 +0100

    Fix typo

--- env/.config/nvim/lua/simbaclaws/plugins/lsp/mason.lua
+++ env/.config/nvim/lua/simbaclaws/plugins/lsp/mason.lua
@@ -27,7 +27,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",
         "html",
         "cssls",
         "tailwindcss",
@@ -48,6 +47,7 @@ return {
         "black", -- python formatter
         "pylint",
         "eslint_d",
+	"typescript-language-server"
       },
     })
   end,
