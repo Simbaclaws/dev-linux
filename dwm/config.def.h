6d456f095b49854b7ae8bec8da8252417b368d2f
simbaclaws <github@hylke.it>
2025-02-25 22:52:50 +0100

    Upgrade browser script and remove asciicam...

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -60,12 +60,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "j4-dmenu-desktop" };
 static const char *termcmd[]  = { "alacritty", NULL };
-
+void
+toggleborder(const Arg *arg) {
+    selmon->sel->bw = selmon->sel->bw == 0 ? borderpx : 0;
+    arrange(selmon);
+}
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+  { MODKEY,                       XK_v,      toggleborder,   {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
