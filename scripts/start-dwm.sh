e4f4a66126d967e26f2461977c93d7e9d595017f
simbaclaws <github@hylke.it>
2025-05-17 16:22:29 +0200

    Trying to get that damned dbus session to work with password prompts...

--- scripts/start-dwm.sh
+++ scripts/start-dwm.sh
@@ -2,4 +2,9 @@
 
 [ -z "$DBUS_SESSION_BUS_ADDRESS" ] && eval $(/usr/bin/dbus-launch --exit-with-session --sh-syntax)
 dbus-update-activation-environment --verbose --all
+eval $(/usr/bin/gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+export GPG_AGENT_INFO
+export GNOME_KEYRING_CONTROL
+export GNOME_KEYRING_PID
 exec /usr/local/bin/dwm 
