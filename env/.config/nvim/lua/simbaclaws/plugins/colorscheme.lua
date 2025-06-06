58a17e41722031bb332e8e8484d92774d3228763
simbaclaws <github@hylke.it>
2025-06-06 19:01:30 +0200

    Change colorschemes... (testing)

--- env/.config/nvim/lua/simbaclaws/plugins/colorscheme.lua
+++ env/.config/nvim/lua/simbaclaws/plugins/colorscheme.lua
@@ -1,9 +1,60 @@
 return {
-    "sly-harvey/radium.nvim",             --- Radium Colorscheme
+    "marko-cerovac/material.nvim",             --- Radium Colorscheme
     lazy = false,
     priority = 1000,
     config = function()
+      require('material').setup {
+        contrast = {
+          terminal = false,
+          sidebars = false,
+          floating_windows = false,
+          cursor_line = false,
+          lsp_virtual_text = false,
+          non_current_windows = false,
+          filetypes = {}
+        },
+
+        styles = {
+          comments = { [[ italic = true ]] },
+          strings = { [[ bold = true ]] },
+          keywords = { [[ underline = true ]] },
+          functions = { [[ bold = true, undercurl = true ]] },
+          variables = {},
+          operators = {},
+          types = {},
+        },
+
+        plugins = {
+          "harpoon",
+          "nvim-cmp",
+          "nvim-tree",
+          "telescope",
+        },
+
+        disable = {
+          colored_cursor = false,
+          borders = false,
+          background = true,
+          term_colors = false,
+          eob_lines = false
+        },
+
+        high_visibility = {
+          lighter = false,
+          darker = true
+        },
+
+        lualine_style = "default",
+
+        async_loading = true,
+
+        custom_colors = nil,
+        
+        custom_highlights = {},
+      }
       -- Load the colorscheme
-      vim.cmd('colorscheme radium')
+      require('material').load()
+      vim.g.material_style = "deep ocean"
+      vim.cmd('colorscheme material')
     end,
 }
